87f3165d5b8e7dd272facea1512d2c5c3b8b70af
danielemaraschin <danikmaraschin@gmail.com>
2021-07-14 13:54:28 +0200

    working with GRID

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -10,11 +10,22 @@ border-radius: 8px;
 `;
 
 const MainGrid = styled.main`
-  display: grid;
+  width: 100%;
   grid-gap: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
   padding: 16px;
+  .profileArea {
+    display: none;
+    @media(min-width: 860px) {
+      display: block;
+    }
+  }
 
   @media(min-width: 860px){
+    max-width: 1110px;
+    display: grid;
     grid-template-areas:
      "profileArea welcomeArea profileRelationsArea";
     grid-template-columns: 
