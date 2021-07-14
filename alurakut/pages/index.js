8ad7a7a604e427c8a2c92576c0e36206b6110754
danielemaraschin <danikmaraschin@gmail.com>
2021-07-14 21:00:17 +0200

    adding js code inside HTML code

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -2,21 +2,20 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 
+const userGithub = 'juniornvieira'
 export default function Home() {
   return (
     <MainGrid>
       <div className="profileArea" style={{ gridArea: 'profileArea' }}>
         <Box >
-          <img src="https://github.com/danielemaraschin.png" style={{ borderRadius: '8px'}}/>
+          <img src={`https://github.com/${userGithub}.png`} style={{ borderRadius: '8px'}}/>
         </Box>
       </div>
       <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
         <Box>
           Bem vindo
         </Box>
-        <Box>
-          Scrapt
-        </Box>
+
       </div>
       <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
         <Box>
