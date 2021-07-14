493b47568ded18325c6b72d94c0af316b6a6fc3f
danielemaraschin <danikmaraschin@gmail.com>
2021-07-14 09:49:46 +0200

    Creating the first function component

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+//const Title = styled.h1`
+//  font-size: 50px;
+//  color: ${({ theme }) => theme.colors.primary};
+//`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <main>
+      Imagem
+      Bem vindo
+      Amigos
+      Comunidades
+    </main>)
 }
