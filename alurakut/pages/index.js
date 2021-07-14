5625238e0dbea6ef982692d02a6a771759a5629a
danielemaraschin <danikmaraschin@gmail.com>
2021-07-14 20:14:07 +0200

    working with MainGrid

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -9,30 +9,6 @@ background: #FFFFFF;
 border-radius: 8px;
 `;
 
-const MainGrid = styled.main`
-  width: 100%;
-  grid-gap: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 500px;
-  padding: 16px;
-  .profileArea {
-    display: none;
-    @media(min-width: 860px) {
-      display: block;
-    }
-  }
-
-  @media(min-width: 860px){
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas:
-     "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 
-      160px 1fr 312px;
-  }
-`;
-
 export default function Home() {
   return (
     <MainGrid>
