f50f91da1af99f81aaa04465c46ddbfd87d9b70d
danielemaraschin <danikmaraschin@gmail.com>
2021-07-14 12:00:31 +0200

    Adding style

--- alurakut/pages/_app.js
+++ alurakut/pages/_app.js
@@ -1,10 +1,25 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+/*reset CSS (necolas reset - reset simples, procurar na internet)*/
+  {
+  margin: 0;
+  passing:0;
+  box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #_next {
+    display: flex;
+    min-eight: 100vh;
+    flex-direction:column;
   }
 `
 
