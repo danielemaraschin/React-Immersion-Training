291b9e8465bd7638c901757e80b13f8d1f93c777
danielemaraschin <danikmaraschin@gmail.com>
2021-07-14 10:03:42 +0200

    Adding style

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 //  font-size: 50px;
 //  color: ${({ theme }) => theme.colors.primary};
 //`
+const Box = styled.div`
+background: #FFFFFF;
+border-radius: 8px;
+`;
 
 export default function Home() {
   return(
