6d43424106b96d68ca821e169c564a8782b77cc8
danielemaraschin <danikmaraschin@gmail.com>
2021-07-15 16:17:48 +0200

    working with .map

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -10,27 +10,46 @@ function ProfileSidebar(propriedades) {
   )
 }
 
-const usuarioAleatorio = "danielemaraschin"
 export default function Home() {
+  const usuarioAleatorio = "danielemaraschin";
+  const pessoasFavoritas = [
+    'juniornvieira',
+    'nkneip',
+    'phr1990',
+    'peas',
+    'omariosouto',
+    'juunegreiros',
+    'felipefialho',
+    'marcobrunodev'
+  ]
   return (
     <>
-      <AlurakutMenu/>
+      <AlurakutMenu />
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-          <ProfileSidebar githubUser={usuarioAleatorio}/>
+          <ProfileSidebar githubUser={usuarioAleatorio} />
         </div>
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>
             Bem vindo
           </Box>
-
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
           <Box>
-            Amigos
-          </Box>
-          <Box>
-            Comunidades
+            <h2 className="smallTitle">
+              Pessoas da Comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <img src={`https://github.com/${itemAtual}.png`} />
+                    <span>{itemAtual}</span>
+                  </a>
+                )
+              })}
+
+            </ul>
           </Box>
         </div>
 
