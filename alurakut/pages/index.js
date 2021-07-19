fb2cf9a71403874947e6480500f6b84822f281d4
danielemaraschin <danikmaraschin@gmail.com>
2021-07-19 11:42:57 +0200

    Working with async await

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -104,12 +104,24 @@ export default function Home() {
               console.log('Campo: ', dadosDoForm.get('image'));
 
               const comunidade = {
-                id: new Date().toISOString(),
                 title: dadosDoForm.get('title'),
                 image: dadosDoForm.get('image'),
+                creatorSlug: usuarioAleatorio,
               }
-              const comunidadesAtualizadas = [...comunidades, comunidade];
-              setComunidades(comunidadesAtualizadas)
+
+              fetch('api/communities', {
+                method: 'POST',
+                headers: {
+                  'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(comunidade)
+              })
+              .then(async (response) => {
+                const dados = await response.json();
+                console.log(dados.registroCriado);
+                const
+                })
+
             }}>
               <div>
                 <input
