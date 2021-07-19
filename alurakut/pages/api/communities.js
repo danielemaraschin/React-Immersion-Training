4e1d13d8bcaf59b151087702c9709748ff69e5b9
danielemaraschin <danikmaraschin@gmail.com>
2021-07-19 11:16:45 +0200

    Working with datoCMS

--- alurakut/pages/api/communities.js
+++ alurakut/pages/api/communities.js
@@ -1,13 +1,29 @@
-import {siteClient} from 'datocms-client';
+import { siteClient } from 'datocms-client';
 
 export default async function recebedorDeRequests(request, response) {
-    const TOKEN = 'e535fbac6981ebbf0916de5b341c39';
-    const client = new SiteClient(TOKEN);
 
-    console.log("token: ",TOKEN);
-    response.json({
-        dados: 'response em JSON'
+    if (request.method === 'POST') {
+        const TOKEN = 'e535fbac6981ebbf0916de5b341c39';
+        const client = new SiteClient(TOKEN);
+
+        const registroCriado = await client.items.create({
+            itemType: "975901", //sempre "string"...é o id do model no datocms, pq ele não correlaciona com o nome communities e sim com o id do model communities - pegar nos settings do model
+            title: "comunidade teste",
+            imageUrl: "https://github.com/danielemaraschin.png",
+            creatorSlug: "danielemaraschin"
+        })
+
+        console.log(registroCriado);
+        response.json({
+            dados: 'response em JSON',
+            registroCriado: registroCriado,
+        })
+        return;
+    }
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, mas no post sim.'
     })
+
 }
 
 // nome desse arquivo será o endereco HTTP: locahost:3000/nomeDesseArquivo
