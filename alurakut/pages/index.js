6bbd0b3a88ab51e18414e210ea36beab406ddfd0
danielemaraschin <danikmaraschin@gmail.com>
2021-07-19 19:23:40 +0200

    Learning about getServerSideProps

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -32,8 +32,8 @@ function ProfileRelationsBox(propriedades) {
 
 
 
-export default function Home() {
-  const usuarioAleatorio = "danielemaraschin";
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser;
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'juniornvieira',
@@ -185,4 +185,30 @@ export default function Home() {
         </div>
       </MainGrid>
     </>)
+}
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+        Authorization: token
+      }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, // will be passed to the page component as props
+  }
 }
