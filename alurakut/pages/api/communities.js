364a5584f590a56b06f9b182dc09a0be9453d43f
danielemaraschin <danikmaraschin@gmail.com>
2021-07-19 10:39:19 +0200

    working with DatoCMS, token, ...

--- alurakut/pages/api/communities.js
+++ alurakut/pages/api/communities.js
@@ -1,8 +1,14 @@
+import {siteClient} from 'datocms-client';
 
 export default async function recebedorDeRequests(request, response) {
     const TOKEN = 'e535fbac6981ebbf0916de5b341c39';
+    const client = new SiteClient(TOKEN);
+
     console.log("token: ",TOKEN);
     response.json({
         dados: 'response em JSON'
     })
 }
+
+// nome desse arquivo será o endereco HTTP: locahost:3000/nomeDesseArquivo
+//http://localhost:3000/api/communities
